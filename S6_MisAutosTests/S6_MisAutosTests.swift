4f5f0e6103683bdb48bf09b9a05a6e7c339cb926
Jass1321 <jassyra13216@gmail.com>
2021-11-03 22:14:12 -0500

    Initial Commit

---
+++ S6_MisAutosTests/S6_MisAutosTests.swift
@@ -0,0 +1,33 @@
+//
+//  S6_MisAutosTests.swift
+//  S6_MisAutosTests
+//
+//  Created by MAC06 on 3/11/21.
+//
+
+import XCTest
+@testable import S6_MisAutos
+
+class S6_MisAutosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ S6_MisAutosUITests/S6_MisAutosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  S6_MisAutosUITestsLaunchTests.swift
+//  S6_MisAutosUITests
+//
+//  Created by MAC06 on 3/11/21.
+//
+
+import XCTest
+
+class S6_MisAutosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
